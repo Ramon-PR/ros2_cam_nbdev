02021cb443409efd6ec667ff91c71a5cbc343c1e
Ramon Pozuelo Ruiz <pozueloruizramon@gmail.com>
2026-02-24 15:08:06 +0100

    Bash script to setup the project

---
+++ prepare_project.bash
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# Remove the template README.md file, since we will create our own.
+rm README.md
+
+# Start a uv project where I can easily install dependencies. It creates a pyproject.toml file
+uv init
+
+# Add a local nbdev to the project.
+uv add nbdev
+
+# Remove uv pyproject.toml, since we will use the one from nbdev, 
+# which is more complete and has the right dependencies.
+rm pyproject.toml
+
+# Create the nbdev project (Adds its own pyproject.toml which can be updated easily with uv add)
+# uv run nbdev-new to use the nbdev locally installed by uv in .venv
+uv run nbdev-new
+
